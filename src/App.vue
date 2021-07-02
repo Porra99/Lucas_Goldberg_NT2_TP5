61eb334f8dbb7e3ca7546f47d0a964ee7240b9ec
Lucasgoldberg99 <lucasgoldberg99@gmail.com>
2021-07-01 22:16:39 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="app">
+    <Header :color="hPickedColor" :backgroundColor="hBackgroundColor"></Header>
+    <Navbar :message="nMessage" :messageColor="nMessageColor" :reset="nReset" v-on:restart="restart"
+               v-on:changeToEasy="changeToEasy()" v-on:changeToHard="changeToHard()"></Navbar>
+    <div id="container">
+      <Game v-on:start="start" v-on:win="win" v-on:miss="miss" ref="game"></Game>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+import Navbar from "./components/Navbar"
+import Game from "./components/Game.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Navbar,
+    Game
+  },
+  data() {
+    return {
+      hPickedColor: '',
+      hBackgroundColor: null,
+      nMessage: '',
+      nReset: 'New colors',
+      nMessageColor: '#fff',
+      isHard: true
+    }
+  },
+  methods: {
+    start(payload) {
+      this.hPickedColor = payload.hPickedColor
+      this.nMessage = payload.nMessage
+      this.nReset = 'New colors'
+      this.hBackgroundColor = 'steelblue'
+    },
+    win(payload) {
+      this.nMessage = payload.nMessage
+      this.nMessageColor = '#000'
+      this.nReset = payload.nReset
+      this.hBackgroundColor = payload.winColor
+    },
+    miss(payload) {
+      this.nMessage = payload.nMessage
+      this.nMessageColor = '#000'
+    },
+    restart() {
+      this.$refs.game.start(this.isHard ? 6 : 3, this.isHard)
+    },
+    changeToEasy() {
+      if (this.isHard) {
+        this.isHard = false;
+        this.$refs.game.changeToEasy()
+      }
+    },
+    changeToHard() {
+      if (!this.isHard) {
+        this.isHard = true
+        this.$refs.game.changeToHard()
+      }
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  background: #232323;
+  margin: 0;
+  font-family: "Montserrat", "Avenir", serif;
+}
+h1 {
+  font-weight: normal;
+  line-height: 1.1;
+  padding: 20px 0;
+}
+#container {
+  margin: 20px auto;
+  max-width: 600px;
+}
+</style>
---
+++ src/components/ColorItems.vue
@@ -0,0 +1,79 @@
+<template>
+
+  <div>
+    <div class="colorItems" :style="{backgroundColor: currentColor ? currentColor : color, display: display}"
+         v-on:click="clickSquare">
+    </div>
+  </div>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'ColorItems',
+    props: {
+    color: {
+      type: String
+    },
+    pickedColor: {
+      type: String
+    },
+    currentColor: {
+      type: String
+    },
+    display: {
+      type: String
+    }
+  },
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+    clickSquare(e) {
+      let clickedColor = e.target.style.backgroundColor;
+      if (clickedColor === this.pickedColor) {
+        this.$emit('win', {
+          nMessage: 'You Picked Right!',
+          squareColors: this.pickedColor,
+          nReset: 'Play Again!',
+          hBackgroundColor: this.pickedColor
+        })
+      } else {
+        this.$emit('changeSquareColor', {
+          square: e.target,
+          color: '#232323'
+        })
+        this.$emit('miss', {
+          nMessage: "Try Again!",
+          nMessageColor: "#000000"
+        })
+      }
+    }
+  },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .colorItems {
+  width: 30%;
+  background: blue;
+  padding-bottom: 30%;
+  float: left;
+  margin: 1.66%;
+  border-radius: 10%;
+  transition: background 0.8s;
+  -webkit-transition: background 0.8s;
+  -moz-transition: background 0.8s;
+  }
+</style>
